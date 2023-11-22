9afc89458f0de559d71d6f2fade8fb324161a682
Jackie <jackieli3576@gmail.com>
2023-11-21 21:29:18 -0600

    Added more explanation of AutoFunctions
    Accounted for drivetrain gear ratio in autoFunction equations
    (testing needed)

--- src/subsystemFiles/autoFunctions.cpp
+++ src/subsystemFiles/autoFunctions.cpp
@@ -2,6 +2,7 @@
 #include "autoFunction.h"
 double wheelCir = 10.21;
 double robotDiameter = 11.5;
+double gearRatio = 36/60;
 void allStop(){
     leftUp.move(0);
     leftMiddle.move(0);
@@ -10,16 +11,17 @@ void allStop(){
     rightMiddle.move(0);
     rightDown.move(0);
 }
-
+/*Stops drive train for certain amount of time in milliseconds*/
 void timeStop(double tiempo){
     // tiempo = time in milliseconds
     allStop();
     pros::delay(tiempo);
 }
-
+/*Drives forward/backwards for a certain distance in inches and at a certain percentage of full speed*/
 void driveE(double speed, double distance){
-    //speed = speed percentage out of 100
-    double degree =-(distance/wheelCir)*360;
+    //speed = speed percentage 
+    //distance = distance in inches
+    double degree =-(distance/wheelCir)*360*gearRatio;
     double rpm = 2*speed;
     if (speed>100 || speed<0){
         return;
@@ -37,16 +39,16 @@ void driveE(double speed, double distance){
     timeStop(20);
     
 }
-
+/*Turns robot a certain degrees at a certain speed in percentage*/
 void turnE(double speed, double rotate){
     //speed = speed percentage
-    //rotate = degrees
+    //rotate = degrees Negative: CounterClockWise | Positive ClockWise
     //diameter = 11.5 inches
 
     if (speed>100 || speed<0){
         return;
     }  
-    double degree = 3.538*(-rotate/2); // diameter of robot / wheel diameter
+    double degree = 3.538*(-rotate/2)*gearRatio; // diameter of robot / wheel diameter
     double rpm = 2*speed;
 
     leftUp.move_relative(degree,rpm);
@@ -62,22 +64,22 @@ void turnE(double speed, double rotate){
     timeStop(20);
 
 }
-
+/*Makes robot turn in a circle with certain outside and inside radius, speed in percentage, and direction*/
 void curveE(double out, double in, double speed, double dir){
     //out = outer arc inches
     //in = inner arc
     //speed = speed percentage
-    //dir = direcition +-
+    //dir = direction +-
     //left negative right positive
     if (speed>100 || speed<0){
         return;
     }
-    if (dir!=1||dir!=-1){
+    if (dir!= 1 || dir!=-1){
         return;
     }
 
-    double outDegree = (out/wheelCir)*360;
-    double inDegree = (in/wheelCir)*360;
+    double outDegree = (out/wheelCir)*360*gearRatio;
+    double inDegree = (in/wheelCir)*360*gearRatio;
     double rpm = speed*2;
     double tiempo = (out/wheelCir)/(rpm/60);
     double speedIn = (in/wheelCir)/tiempo;
